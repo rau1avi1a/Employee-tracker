38471b5abd9b6c569b5e10811111cf2a6ef8b400
rau1avi1a <avila0118@icloud.com>
2023-06-18 21:30:43 -0700

    added functionality to view departments, roles, and employees

--- server.js
+++ server.js
@@ -10,34 +10,72 @@ const db = mysql.createConnection(
     console.log('Connected to the employee database...')
 );
 
-async function displayDepartments() {
 
-}
+//done.. this function displays departments
+function displayDepartments() {
+    console.log('Viewing all departments...');
 
-async function displayRoles() {
+    const sql = 'SELECT name AS department FROM department'
 
-}
+    db.query(sql, function (err, results) {
+        console.table(results);
+      });
+};
 
-async function displayEmployees() {
+//done.. this function displays roles
+function displayRoles() {
+    console.log('Viewing all roles...')
 
-}
+    const sql = 'SELECT title, salary FROM role JOIN department ON role.department_id = department.id'
 
+    db.query(sql, function (err, results) {
+        console.table(results);
+      });
+};
+
+//this function displays employees
+function displayEmployees() {
+    console.log('Viewing all employees...')
+
+    const sql = 'SELECT first_name, last_name, manager_id FROM employee JOIN role ON employee.role_id = role.id'
+
+    db.query(sql, function (err, results) {
+        console.table(results);
+      });
+};
+
+//this function adds a department
 async function addDepartment() {
+    const params = await inquirer.prompt([{
+        message: 'What department would you like to add?',
+        name: 'department',
+        type: 'input'
+    }]);
 
-}
+    const sql = 'INSERT INTO department (name) VALUES (?)'
 
-async function addRole() {
+    db.query(sql, params, (err, results) => {
+        console.table(results);
+    });
 
-}
+};
 
-async function addEmployee() {
+//this function adds a role
+function addRole() {
 
-}
+};
 
-async function updateEmployee() {
+//this function adds an employee
+function addEmployee() {
 
-}
+};
 
+//this function updates employee information
+function updateEmployee() {
+
+};
+
+//init function
 async function handleOptions() {
 
     const options = [
@@ -55,26 +93,27 @@ async function handleOptions() {
         name: 'choice',
         type: 'list',
         choices: options,
-    }])
-    if (results == 'View All Departments') {
+    }]);
+
+    if (results.choice == 'View All Departments') {
         displayDepartments();
         handleOptions();
-    } else if (results == 'View All Roles') {
+    } else if (results.choice == 'View All Roles') {
         displayRoles();
         handleOptions();
-    } else if (results == 'View All Employees') {
+    } else if (results.choice == 'View All Employees') {
         displayEmployees();
         handleOptions();
-    } else if (results == 'Add Department') {
+    } else if (results.choice == 'Add Department') {
         addDepartment();
         handleOptions();
-    } else if (results == 'Add Role') {
+    } else if (results.choice == 'Add Role') {
         addRole();
         handleOptions();
-    } else if (results == 'Add Employee') {
+    } else if (results.choice == 'Add Employee') {
         addEmployee();
         handleOptions();
-    } else if (results == 'Update Employee Role') {
+    } else if (results.choice == 'Update Employee Role') {
         updateEmployee();
         handleOptions();
     }
