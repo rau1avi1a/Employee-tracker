5e61fb3d49dbee64fd8c5393fda17beb9462d97f
rau1avi1a <avila0118@icloud.com>
2023-06-18 20:03:51 -0700

    added working data for database

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP IF EXISTS employees_db;
+DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
@@ -10,19 +10,21 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
-    FOREIGN KEY(department_id)
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name,
-    last_name,
-    role_id,
-    manager_id
-
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE CASCADE
 );
--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'luna',
+        database: 'employees_db'
+    },
+    console.log('Connected to the employee database...')
+);
 
 async function displayDepartments() {
-    //connect to mysql
+
+}
+
+async function displayRoles() {
+
+}
+
+async function displayEmployees() {
+
+}
+
+async function addDepartment() {
+
+}
+
+async function addRole() {
+
+}
+
+async function addEmployee() {
+
+}
+
+async function updateEmployee() {
+
 }
 
 async function handleOptions() {
-    
+
     const options = [
         'View All Departments',
         'View All Roles',
@@ -15,10 +48,10 @@ async function handleOptions() {
         'Add Role',
         'Add Employee',
         'Update Employee Role'
-    ]
+    ];
 
     const results = await inquirer.prompt([{
-        message: 'What would you liike to do?',
+        message: 'What would you like to do?',
         name: 'choice',
         type: 'list',
         choices: options,
@@ -26,7 +59,25 @@ async function handleOptions() {
     if (results == 'View All Departments') {
         displayDepartments();
         handleOptions();
-    } 
-}
+    } else if (results == 'View All Roles') {
+        displayRoles();
+        handleOptions();
+    } else if (results == 'View All Employees') {
+        displayEmployees();
+        handleOptions();
+    } else if (results == 'Add Department') {
+        addDepartment();
+        handleOptions();
+    } else if (results == 'Add Role') {
+        addRole();
+        handleOptions();
+    } else if (results == 'Add Employee') {
+        addEmployee();
+        handleOptions();
+    } else if (results == 'Update Employee Role') {
+        updateEmployee();
+        handleOptions();
+    }
+};
 
 handleOptions();
